c1e002b5f11b45bd41a4265758fafb8ed27c89c3
violettomania <danielpolya@protonmail.com>
2024-01-23 22:30:08 +0100

    setup project with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <nav className='bg-emerald-100'>
+      <p>it works!</p>
+    </nav>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/* eslint-env node */
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.tsx'),
+  output: {
+    filename: 'app.bundle.js',
+    path: path.join(__dirname, '_site'),
+    publicPath: './',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // Match .ts and .tsx files
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/, // Match .js files
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/, // Match .css files
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: './',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+};
