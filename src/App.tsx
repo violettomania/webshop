a3c9cfdb8ac8617eadce71e11ffff48f65099d10
violettomania <danielpolya@protonmail.com>
2024-01-26 12:01:56 +0100

    amend Hero component with carousel

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,6 @@ export default function App() {
       <section className='align-element py-20'>
         <div className='grid lg:grid-cols-2 gap-24 items-center'>
           <Hero />
-          <div className='hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box '>
-            <div className='carousel-item'>
-              <img
-                src='/src/assets/hero1.webp'
-                className='rounded-box h-full w-80 object-cover'
-                alt='product'
-              />
-            </div>
-            <div className='carousel-item'>
-              <img
-                src='/src/assets/hero2.webp'
-                className='rounded-box h-full w-80 object-cover'
-                alt='product'
-              />
-            </div>
-            <div className='carousel-item'>
-              <img
-                src='/src/assets/hero3.webp'
-                className='rounded-box h-full w-80 object-cover'
-                alt='product'
-              />
-            </div>
-            <div className='carousel-item'>
-              <img
-                src='/src/assets/hero4.webp'
-                className='rounded-box h-full w-80 object-cover'
-                alt='product'
-              />
-            </div>
-          </div>
         </div>
         <div className='pt-24'>
           <div className='border-b border-base-300 pb-5'>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,51 @@
 export default function Hero() {
   return (
-    <div>
-      <h1 className='max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl'>
-        We are changing the way people shop
-      </h1>
-      <p className='mt-8 max-w-xl text-lg leading-8'>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
-        repellat explicabo enim soluta temporibus asperiores aut obcaecati
-        perferendis porro nobis.
-      </p>
-      <div className='mt-10'>
-        <a className='btn btn-primary' href='/products'>
-          Our Products
-        </a>
+    <>
+      <div>
+        <h1 className='max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl'>
+          We are changing the way people shop
+        </h1>
+        <p className='mt-8 max-w-xl text-lg leading-8'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+          repellat explicabo enim soluta temporibus asperiores aut obcaecati
+          perferendis porro nobis.
+        </p>
+        <div className='mt-10'>
+          <a className='btn btn-primary' href='/products'>
+            Our Products
+          </a>
+        </div>
       </div>
-    </div>
+      <div className='hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box '>
+        <div className='carousel-item'>
+          <img
+            src='/src/assets/hero1.webp'
+            className='rounded-box h-full w-80 object-cover'
+            alt='product'
+          />
+        </div>
+        <div className='carousel-item'>
+          <img
+            src='/src/assets/hero2.webp'
+            className='rounded-box h-full w-80 object-cover'
+            alt='product'
+          />
+        </div>
+        <div className='carousel-item'>
+          <img
+            src='/src/assets/hero3.webp'
+            className='rounded-box h-full w-80 object-cover'
+            alt='product'
+          />
+        </div>
+        <div className='carousel-item'>
+          <img
+            src='/src/assets/hero4.webp'
+            className='rounded-box h-full w-80 object-cover'
+            alt='product'
+          />
+        </div>
+      </div>
+    </>
   );
 }
