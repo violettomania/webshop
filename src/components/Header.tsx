8a9cb35f5e5c2b0de7d1404feb0888f4ad4e7d77
violettomania <danielpolya@protonmail.com>
2024-01-26 13:38:36 +0100

    update Header component with logged in user content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,3 +14,16 @@ export default function Header() {
     </header>
   );
 }
+
+// const renderLoggedInUserHeader = () => {
+//   return (
+//     <header className='bg-neutral py-2 text-neutral-content'>
+//       <div className='align-element flex justify-center sm:justify-end'>
+//         <div className='flex gap-x-2 sm:gap-x-8 items-center'>
+//           <p className='text-xs sm:text-sm'>Hello, demo user</p>
+//           <button className='btn btn-xs btn-outline btn-primary'>logout</button>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
