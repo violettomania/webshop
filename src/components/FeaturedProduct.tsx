fc111332292c20c674a7931b2aae32ed3d3acd04
violettomania <danielpolya@protonmail.com>
2024-01-30 00:17:58 +0100

    feature: add props for FeaturedProduct component

--- src/components/FeaturedProduct.tsx
+++ src/components/FeaturedProduct.tsx
@@ -18,16 +18,15 @@ export default function FeaturedProduct({
     >
       <figure className='px-4 pt-4'>
         <img
-          src='https://images.pexels.com/photos/943150/pexels-photo-943150.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=1600'
-          alt='avant-garde lamp'
+          src={image}
+          alt={title}
+          aria-label={title}
           className='rounded-xl h-64 md:h-48 w-full object-cover'
         />
       </figure>
       <div className='card-body items-center text-center'>
-        <h2 className='card-title capitalize tracking-wider'>
-          avant-garde lamp
-        </h2>
-        <span className='text-secondary'>$179.99</span>
+        <h2 className='card-title capitalize tracking-wider'>{title}</h2>
+        <span className='text-secondary'>{price}</span>
       </div>
     </a>
   );
