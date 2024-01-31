0a1b4cfe91fd64e9e4059a6702e49e7a8a8bcd39
violettomania <danielpolya@protonmail.com>
2024-01-31 23:15:03 +0100

    bugfix: carousel images not showing on Hero; chore: add TODO

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,34 +1,25 @@
+import hero1 from '../assets/hero1.webp';
+import hero2 from '../assets/hero2.webp';
+import hero3 from '../assets/hero3.webp';
+import hero4 from '../assets/hero4.webp';
+
+const carouselImages = [hero1, hero2, hero3, hero4];
+
+// TODO: fix carousel images not changing
 export default function Carousel() {
   return (
     <div className='hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box '>
-      <div className='carousel-item'>
-        <img
-          src='/src/assets/hero1.webp'
-          className='rounded-box h-full w-80 object-cover'
-          alt='product'
-        />
-      </div>
-      <div className='carousel-item'>
-        <img
-          src='/src/assets/hero2.webp'
-          className='rounded-box h-full w-80 object-cover'
-          alt='product'
-        />
-      </div>
-      <div className='carousel-item'>
-        <img
-          src='/src/assets/hero3.webp'
-          className='rounded-box h-full w-80 object-cover'
-          alt='product'
-        />
-      </div>
-      <div className='carousel-item'>
-        <img
-          src='/src/assets/hero4.webp'
-          className='rounded-box h-full w-80 object-cover'
-          alt='product'
-        />
-      </div>
+      {carouselImages.map((image) => {
+        return (
+          <div key={image} className='carousel-item'>
+            <img
+              src={image}
+              className='rounded-box h-full w-80 object-cover'
+              alt='carousel-item'
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
