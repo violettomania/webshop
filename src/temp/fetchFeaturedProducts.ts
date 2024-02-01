01637dba5fcea230d986bb929ae636eb07e642a6
violettomania <147735091+violettomania@users.noreply.github.com>
2024-02-01 16:19:50 +0100

    Create temp fetchFeaturedProducts.ts

---
+++ src/temp/fetchFeaturedProducts.ts
@@ -0,0 +1,55 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// TODO: make this a generic type?
+// TODO: replace FeatureProductProps
+interface FeaturedProduct {
+  id: string;
+  attributes: {
+    title: string;
+    image: string;
+    price: string;
+  };
+}
+
+// TODO: move this to a config file
+const url = 'https://strapi-store-server.onrender.com/api/products';
+const featuredQuery = '?featured=true';
+
+// example urls
+// const featuredItemsUrl =
+//   'https://strapi-store-server.onrender.com/api/products?featured=true';
+// const itemsUrl = 'https://strapi-store-server.onrender.com/api/products';
+// const itemUrl = 'https://strapi-store-server.onrender.com/api/products/12';
+
+// TODO: enforce type
+export const fetchFeaturedProducts = createAsyncThunk(
+  'products/fetchFeaturedProducts',
+  async () => {
+    try {
+      const response = await fetch(`${url}${featuredQuery}`);
+      const data = await response.json();
+      const { data: products } = data;
+      if (products) {
+        return products.map((product: FeaturedProduct) => {
+          const {
+            id,
+            attributes: { title, image, price },
+          } = product;
+          return {
+            id: id,
+            title: title,
+            image: image,
+            price: price,
+          };
+        });
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
+
+// TODO: rewrite to axios
