c4049fc17f0b534f67febd1c9d72724e123de139
violettomania <danielpolya@protonmail.com>
2024-02-02 18:42:20 +0100

    feature: add 404 error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom';
+
 export default function ErrorPage() {
   return (
-    <div>
-      <h1>Error: Page not found</h1>
-    </div>
+    <main className='grid min-h-[100vh] place-items-center px-8'>
+      <div className='text-center'>
+        <p className='text-9xl font-semibold text-primary'>404</p>
+        <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+          page not found
+        </h1>
+        <p className='mt-6 text-lg leading-7'>
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className='mt-10'>
+          <Link className='btn btn-secondary' to='/'>
+            go back home
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
