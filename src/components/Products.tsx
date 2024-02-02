20a0858d9d6153cd429ae0fb604b5149f19b5640
violettomania <danielpolya@protonmail.com>
2024-02-02 19:07:07 +0100

    refactor: change svg to icons, chore: remoe TODOs

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,6 +5,7 @@ import ProductsFilter from './ProductsFilter';
 import FeaturedProduct from './FeaturedProduct';
 import { FeaturedProductType } from '../slices/allProductsSlice';
 import Loading from './Loading';
+import { BsFillGridFill, BsList } from 'react-icons/bs';
 
 export default function Products() {
   const dispatch = useAppDispatch();
@@ -17,7 +18,6 @@ export default function Products() {
     dispatch(fetchAllProducts());
   }, [dispatch]);
 
-  // TODO: change svgs to icons
   // TODO: bugfix: products are not displayed
   return loading ? (
     <Loading />
@@ -31,36 +31,13 @@ export default function Products() {
             type='button'
             className='text-xl btn btn-circle btn-sm btn-primary text-primary-content'
           >
-            <svg
-              stroke='currentColor'
-              fill='currentColor'
-              stroke-width='0'
-              viewBox='0 0 16 16'
-              height='1em'
-              width='1em'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path d='M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z'></path>
-            </svg>
+            <BsFillGridFill />
           </button>
           <button
             type='button'
             className='text-xl btn btn-circle btn-sm btn-ghost text-based-content'
           >
-            <svg
-              stroke='currentColor'
-              fill='currentColor'
-              stroke-width='0'
-              viewBox='0 0 16 16'
-              height='1em'
-              width='1em'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                fill-rule='evenodd'
-                d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z'
-              ></path>
-            </svg>
+            <BsList />
           </button>
         </div>
       </div>
