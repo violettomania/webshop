5a7f7011aaf2fe97e9b6192c5c50dc9cc830596d
violettomania <danielpolya@protonmail.com>
2024-02-05 23:01:38 +0100

    refactor: reorder variables

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -11,9 +11,9 @@ const gridDisplayClasses = 'pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3';
 const listDisplayClasses = 'mt-12 grid gap-y-8';
 const listCardDisplayClasses =
   'p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 flex-wrap  bg-base-100 shadow-xl hover:shadow-2xl duration-300 group';
-const selectedPageButtonClasses = 'bg-base-300 border-base-300';
 const gridCardDisplayClasses =
   'card w-full shadow-xl hover:shadow-2xl transition duration-300';
+const selectedPageButtonClasses = 'bg-base-300 border-base-300';
 
 export default function Products() {
   const dispatch = useAppDispatch();
