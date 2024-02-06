3b02da83c4a89ce976d85442f427020c594a57a1
violettomania <danielpolya@protonmail.com>
2024-02-06 14:24:36 +0100

    refactor: pagination separate component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+
+interface PaginationProps {
+  pageCount: number;
+  onPageNumberChange: (e: React.MouseEvent, page: number) => void;
+}
+
+const selectedPageButtonClasses = 'bg-base-300 border-base-300';
+
+export default function Pagination({
+  pageCount,
+  onPageNumberChange,
+}: PaginationProps) {
+  const [currentPage, setCurrentPage] = useState(
+    Number(localStorage.getItem('page')) || 1
+  );
+
+  const handlePageNumberChange = (event: React.MouseEvent, page: number) => {
+    event.preventDefault();
+    onPageNumberChange(event, page);
+    setCurrentPage(page);
+    localStorage.setItem('page', String(page)); // TODO: refactor: use a function
+  };
+
+  const handleNextPage = (event: React.MouseEvent) => {
+    event.preventDefault();
+    const nextPage = currentPage + 1 > pageCount ? 1 : currentPage + 1;
+    onPageNumberChange(event, nextPage);
+    setCurrentPage(nextPage);
+    localStorage.setItem('page', String(nextPage));
+  };
+
+  const handlePrevPage = (event: React.MouseEvent) => {
+    event.preventDefault();
+    const prevPage =
+      currentPage - 1 < pageCount && currentPage - 1 > 0
+        ? currentPage - 1
+        : pageCount;
+    onPageNumberChange(event, prevPage);
+    setCurrentPage(prevPage);
+    localStorage.setItem('page', String(prevPage));
+  };
+
+  return (
+    <div className='mt-16 flex justify-end'>
+      <div className='join'>
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={handlePrevPage}
+        >
+          Prev
+        </button>
+        {Array.from({ length: pageCount }, (_, i) => i + 1).map((page) => (
+          <button
+            onClick={(e) => handlePageNumberChange(e, page)}
+            key={page}
+            className={`btn btn-xs sm:btn-md border-none join-item ${
+              currentPage === page ? selectedPageButtonClasses : ''
+            }`}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={handleNextPage}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -7,6 +7,7 @@ import ProductCard from './ProductCard';
 import Loading from './Loading';
 import { ProductCardType } from '../slices/featuredProductsSlice';
 import ProductsLayoutToggle from './ProductsLayoutToggle';
+import Pagination from './Pagination';
 
 const gridDisplayClasses = 'pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3';
 const listDisplayClasses = 'mt-12 grid gap-y-8';
@@ -14,7 +15,6 @@ const listCardDisplayClasses =
   'p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 flex-wrap  bg-base-100 shadow-xl hover:shadow-2xl duration-300 group';
 const gridCardDisplayClasses =
   'card w-full shadow-xl hover:shadow-2xl transition duration-300';
-const selectedPageButtonClasses = 'bg-base-300 border-base-300';
 
 export default function Products() {
   const dispatch = useAppDispatch();
@@ -46,28 +46,8 @@ export default function Products() {
 
   const handlePageNumberChange = (event: React.MouseEvent, page: number) => {
     event.preventDefault();
-    dispatch(fetchProducts(page));
     setCurrentPage(page);
-    localStorage.setItem('page', String(page)); // TODO: refactor: use a function
-  };
-
-  const handleNextPage = (event: React.MouseEvent) => {
-    event.preventDefault();
-    const nextPage = currentPage + 1 > pageCount ? 1 : currentPage + 1;
-    dispatch(fetchProducts(nextPage));
-    setCurrentPage(nextPage);
-    localStorage.setItem('page', String(nextPage));
-  };
-
-  const handlePrevPage = (event: React.MouseEvent) => {
-    event.preventDefault();
-    const prevPage =
-      currentPage - 1 < pageCount && currentPage - 1 > 0
-        ? currentPage - 1
-        : pageCount;
-    dispatch(fetchProducts(prevPage));
-    setCurrentPage(prevPage);
-    localStorage.setItem('page', String(prevPage));
+    dispatch(fetchProducts(page));
   };
 
   const handleLayoutToggle = (
@@ -110,33 +90,10 @@ export default function Products() {
           ))}
         </div>
       </div>
-      <div className='mt-16 flex justify-end'>
-        <div className='join'>
-          <button
-            className='btn btn-xs sm:btn-md join-item'
-            onClick={handlePrevPage}
-          >
-            Prev
-          </button>
-          {Array.from({ length: pageCount }, (_, i) => i + 1).map((page) => (
-            <button
-              onClick={(e) => handlePageNumberChange(e, page)}
-              key={page}
-              className={`btn btn-xs sm:btn-md border-none join-item ${
-                currentPage === page ? selectedPageButtonClasses : ''
-              }`}
-            >
-              {page}
-            </button>
-          ))}
-          <button
-            className='btn btn-xs sm:btn-md join-item'
-            onClick={handleNextPage}
-          >
-            Next
-          </button>
-        </div>
-      </div>
+      <Pagination
+        pageCount={pageCount}
+        onPageNumberChange={handlePageNumberChange}
+      />
     </section>
   );
 }
