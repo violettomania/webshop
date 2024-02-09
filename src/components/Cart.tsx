12348f263b995ede0a705977114106975a60b323
violettomania <danielpolya@protonmail.com>
2024-02-09 17:01:37 +0100

    WIP refactor Cart: add CartItems component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,5 @@
 import { RootState, useAppSelector } from '../store/store';
-import formatPrice from '../util/priceFormatter';
+import CartItems from './CartItems';
 
 // TODO: add clear cart button
 export default function Cart() {
@@ -21,70 +21,7 @@ export default function Cart() {
             </h2>
           </div>
           <div className='mt-8 grid gap-8 lg:grid-cols-12'>
-            <div className='lg:col-span-8'>
-              {cartItems.map(
-                ({
-                  productID,
-                  image,
-                  title,
-                  price,
-                  company,
-                  productColor,
-                  amount,
-                }) => (
-                  <article
-                    key={productID}
-                    className='mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0'
-                  >
-                    <img
-                      src={image}
-                      alt={title}
-                      className='h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover'
-                    />
-                    <div className='sm:ml-16 sm:w-48'>
-                      <h3 className='capitalize font-medium'>{title}</h3>
-                      <h4 className='mt-2 capitalize text-sm text-neutral-content'>
-                        {company}
-                      </h4>
-                      <p className='mt-4 text-sm capitalize flex items-center gap-x-2'>
-                        color :
-                        <span
-                          className='badge badge-sm'
-                          style={{ backgroundColor: `${productColor}` }}
-                        ></span>
-                      </p>
-                    </div>
-                    <div className='sm:ml-12'>
-                      <div className='form-control max-w-xs'>
-                        <label htmlFor='amount' className='label p-0'>
-                          <span className='label-text'>Amount</span>
-                        </label>
-                        <select
-                          name='amount'
-                          id='amount'
-                          className='mt-2 select select-base select-bordered select-xs'
-                          defaultValue={amount}
-                        >
-                          {Array.from({ length: 20 }, (_, i) => i + 1).map(
-                            (number) => (
-                              <option key={number} value={number}>
-                                {number}
-                              </option>
-                            )
-                          )}
-                        </select>
-                      </div>
-                      <button className='mt-2 link link-primary link-hover text-sm'>
-                        remove
-                      </button>
-                    </div>
-                    <p className='font-medium sm:ml-auto'>{`$${formatPrice(
-                      price
-                    )}`}</p>
-                  </article>
-                )
-              )}
-            </div>
+            <CartItems />
             <div className='lg:col-span-4 lg:pl-4'>
               <div className='card bg-base-200'>
                 <div className='card-body'>
