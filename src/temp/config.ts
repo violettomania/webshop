e81e58c9045afeb527479dee9028b9d95fe63ba5
violettomania <147735091+violettomania@users.noreply.github.com>
2024-02-15 15:55:11 +0100

    Create config.ts

---
+++ src/temp/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  productsUrl: 'https://strapi-store-server.onrender.com/api/products',
+  featuredQuery: '?featured=true',
+  registerUrl:
+    'https://strapi-store-server.onrender.com/api/auth/local/register',
+  loginUrl: 'https://strapi-store-server.onrender.com/api/auth/local',
+  ordersUrl: 'https://strapi-store-server.onrender.com/api/orders',
+};
