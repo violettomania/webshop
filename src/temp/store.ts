3462c04c76d1b1d54ab85ebd53a32519166d3687
violettomania <147735091+violettomania@users.noreply.github.com>
2024-02-15 15:56:07 +0100

    Create store.ts

---
+++ src/temp/store.ts
@@ -0,0 +1,90 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { Middleware } from '@reduxjs/toolkit';
+import productsReducer from '../slices/productsSlice';
+import featuredProductsReducer from '../slices/featuredProductsSlice';
+import ordersReducer from '../slices/ordersSlice';
+import singleProductReducer from '../slices/singleProductSlice';
+import cartReducer from '../slices/cartSlice';
+import userReducer from '../slices/userSlice';
+
+// TODO: create middleware.ts
+const savedCartItems = localStorage.getItem('cartItems');
+const savedTotals = localStorage.getItem('totals');
+
+let totals = savedTotals ? JSON.parse(savedTotals) : null;
+if (
+  Array.isArray(totals) ||
+  !totals ||
+  !(
+    'cartTotal' in totals &&
+    'numItemsInCart' in totals &&
+    'orderTotal' in totals &&
+    'shipping' in totals &&
+    'tax' in totals
+  )
+) {
+  totals = {
+    cartTotal: 0,
+    numItemsInCart: 0,
+    orderTotal: 0,
+    shipping: 0,
+    tax: 0,
+  };
+}
+
+const preloadedState = savedCartItems
+  ? {
+    cart: {
+      cartItems: JSON.parse(savedCartItems),
+      totals: totals,
+    },
+  }
+  : {
+    cart: {
+      cartItems: [],
+      totals: totals,
+    },
+  };
+
+const localStorageMiddleware: Middleware = (storeApi) => (next) => (action) => {
+  let result = next(action);
+  if (action.type === 'user/logoutUser') {
+    localStorage.removeItem('user');
+    localStorage.removeItem('cartItems');
+    localStorage.removeItem('totals');
+    storeApi.dispatch({ type: 'cart/clearCart' });
+  } else {
+    localStorage.setItem(
+      'user',
+      JSON.stringify(storeApi.getState().user.registeredUser)
+    );
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(storeApi.getState().cart.cartItems)
+    );
+    localStorage.setItem(
+      'totals',
+      JSON.stringify(storeApi.getState().cart.totals)
+    );
+  }
+  return result;
+};
+
+const store = configureStore({
+  reducer: {
+    paged: productsReducer,
+    featured: featuredProductsReducer,
+    orders: ordersReducer,
+    single: singleProductReducer,
+    cart: cartReducer,
+    user: userReducer,
+  },
+  preloadedState,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
