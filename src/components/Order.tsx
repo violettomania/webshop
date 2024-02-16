a0e2facafa7cb4ee07de727b951653ed60ce520e
violettomania <danielpolya@protonmail.com>
2024-02-16 17:53:31 +0100

    bugfix: single order renders the whole table

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -18,30 +18,12 @@ export default function SingleOrder({
   const date = formatDate(createdAt);
 
   return (
-    <div className='mt-8'>
-      <h4 className='mb-4 capitalize'>total orders : 2907</h4>
-      <div className='overflow-x-auto'>
-        <table className='table table-zebra'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Address</th>
-              <th>Products</th>
-              <th>Cost</th>
-              <th className='hidden sm:block'>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{name}</td>
-              <td>{address}</td>
-              <td>{numItemsInCart}</td>
-              <td>{orderTotal}</td>
-              <td className='hidden sm:block'>{date}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <tr>
+      <td>{name}</td>
+      <td>{address}</td>
+      <td>{numItemsInCart}</td>
+      <td>{orderTotal}</td>
+      <td className='hidden sm:block'>{date}</td>
+    </tr>
   );
 }
