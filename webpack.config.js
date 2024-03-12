88541073e2056be26174e800eedc70e492e09ea2
violettomania <danielpolya@protonmail.com>
2024-03-12 17:07:16 +0100

    add webp plugin

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.webp$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
