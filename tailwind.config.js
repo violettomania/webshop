2430f28c1025fdbcdcd284e45618b9ae7577b7e0
violettomania <danielpolya@protonmail.com>
2024-03-12 17:26:31 +0100

    configure tailwind for production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
   plugins: [require('daisyui')],
   daisyui: {
     themes: ['winter', 'dracula'],
