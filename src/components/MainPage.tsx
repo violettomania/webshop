01aeae4e66226ffe6a984622c047fa26cb1873f1
violettomania <danielpolya@protonmail.com>
2024-03-12 16:58:13 +0100

    add version

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,11 +1,18 @@
 import FeaturedProducts from './FeaturedProducts';
 import Hero from './Hero';
 
+const version = process.env.REACT_APP_VERSION;
+
 export default function MainPage() {
   return (
-    <section className='align-element py-20'>
-      <Hero />
-      <FeaturedProducts />
-    </section>
+    <>
+      <section className='align-element py-20'>
+        <Hero />
+        <FeaturedProducts />
+      </section>
+      <div className='version-info'>
+        <p>Version: {version}</p>
+      </div>
+    </>
   );
 }
